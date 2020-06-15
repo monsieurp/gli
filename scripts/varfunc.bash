22bc351f568e843a21703b436331ba5c96ca0bc3
Patrice Clement <monsieurp@gentoo.org>
2020-06-15 10:39:07 +0200

    uppercase -> lowercase

--- scripts/varfunc.bash
+++ scripts/varfunc.bash
@@ -5,25 +5,27 @@
 : ${GLI_DEBUG=0}
 : ${GLI_LOG_FILE='/tmp/gli.log'}
 : ${GLI_MOUNT='/mnt/gentoo'}
+
 export GLI_D GLI_BTITLE GLI_DEBUG GLI_LOG_FILE GLI_MOUNT
 
 # Helper function to print debug messages into log file.
 function gli_debug
 {
-    local timestamp=$(date '+%d/%m/%y|%H:%M:%S')
+    local timestamp
+    timestamp=$(date '+%d/%m/%y|%H:%M:%S')
     echo "[${timestamp}] $@" >> ${GLI_LOG_FILE}
 }
 
 # Format dialog --menu arguments.
 function gli_fmt_d_menu
 {
-    local ARGS OUT
-    ARGS=$1
-    OUT=""
+    local args out
+    args=$1
+    out=""
 
     while IFS="|" read -r one two; do
-        OUT="${OUT} '$one' '$two'";
-    done <<< ${ARGS}
+        out="${out} '$one' '$two'";
+    done <<< ${args}
 
-    echo ${OUT}
+    echo ${out}
 }
