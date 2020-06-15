16a2752e23ad9d2924f97f1a4bce05b7b738d68a
Patrice Clement <monsieurp@gentoo.org>
2020-06-15 16:01:40 +0200

    invert Yes and No labels

--- scripts/sshd.bash
+++ scripts/sshd.bash
@@ -7,13 +7,15 @@ ERROR=0
 "${GLI_D}" \
     --title "${GLI_TITLE}" \
     --backtitle "${GLI_BTITLE}" \
+    --yes-label 'No' \
+    --no-label 'Yes' \
     --yesno 'Would you like to start sshd for this installation?
 
 sshd can be started to allow other users
 to access the system during the installation.' 0 0
 RC=$?
 
-if [[ ${RC} -eq 0 ]]; then
+if [[ ${RC} -eq 1 ]]; then
     OUTPUT=$( rc-service sshd start 2>&1 > /dev/null )
     RC=$?
 
@@ -26,13 +28,13 @@ if [[ ${RC} -eq 0 ]]; then
         "${GLI_D}" \
             --title "${GLI_TITLE}" \
             --backtitle "${GLI_BTITLE}" \
-            --msgbox "sshd started successfully!" 0 0
+            --msgbox 'sshd started successfully!' 0 0
     else
         "${GLI_D}" \
             --title "${GLI_TITLE}" \
             --backtitle "${GLI_BTITLE}" \
-            --yes-label "Continue" \
-            --no-label "Restart Installation" \
+            --yes-label 'Continue' \
+            --no-label 'Restart Installation' \
             --yesno "Could not start sshd:
     
     ${OUTPUT}" 0 0
