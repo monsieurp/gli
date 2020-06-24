a32e6ca91a875acd65bbaf9066d08ee782e4a800
Patrice Clement <monsieurp@gentoo.org>
2020-06-24 18:24:52 +0200

    implement SSHDialog

--- gli.py
+++ gli.py
@@ -7,12 +7,14 @@ PALETTE = [
     ('footer',      'white',    'black'),
     ('bgcolor',     'black',    'dark blue'),
     ('wcolor',      'black',    'light gray'),
-    ('selectable',  'white',    'light cyan'),
-    ('focus',       'black',    'light cyan'),
+    ('selectable',  'white',    'dark blue'),
+    ('focus',       'black',    'dark blue'),
     ('esel',        'white',    'dark red'),
     ('efoc',        'black',    'dark red'),
     ('exit',        'white',    'dark red')
 ]
+
+SFILL = '▞'
 
 
 class IntroDialog(urwid.WidgetWrap):
@@ -45,7 +47,7 @@ Would you like to begin the installation with GLI?"""
         content = urwid.AttrMap(urwid.LineBox(content), 'wcolor')
         content = urwid.Overlay(
             content,
-            urwid.AttrMap(urwid.SolidFill('\N{LIGHT SHADE}'), 'bgcolor'),
+            urwid.AttrMap(urwid.SolidFill(SFILL), 'bgcolor'),
             align='center', valign='middle',
             width=('relative', 60),
             height=('relative', 40)
@@ -56,9 +58,53 @@ Would you like to begin the installation with GLI?"""
 
     def handle_input(self, button):
         if button.label == "Yes":
-            self.gli_widget.cycle_dialog(IntroDialog(self.gli_widget))
+            self.gli_widget.cycle_dialog(SSHDialog(self.gli_widget))
         elif button.label == "No":
             raise urwid.ExitMainLoop()
+
+
+class SSHDialog(urwid.WidgetWrap):
+    dialog_text = """Would you like to start the SSH daemon?
+
+sshd can be started to grant other users access
+to this machine whilst performing the installation."""
+
+    def __init__(self, gli_widget):
+        self.gli_widget = gli_widget
+
+        content_frame = urwid.Frame(body=None)
+
+        yes = urwid.AttrMap(urwid.Button('Yes', self.handle_input), 'focus', 'selectable')
+        no = urwid.AttrMap(urwid.Button('No', self.handle_input), 'focus', 'selectable')
+
+        buttons = urwid.GridFlow([no, yes], 10, 3, 1, 'center')
+
+        content = urwid.Text(self.dialog_text)
+        content = urwid.Padding(content,
+            align='center', width='pack'
+        )
+        content = urwid.Pile([content, urwid.Divider(), buttons], focus_item=2)
+        content = urwid.AttrMap(urwid.Filler(content, valign='middle', top=0,
+            bottom=0), 'wcolor')
+        content = urwid.AttrMap(urwid.LineBox(content), 'wcolor')
+        content = urwid.Overlay(
+            content,
+            urwid.AttrMap(urwid.SolidFill(SFILL), 'bgcolor'),
+            align='center', valign='middle',
+            width=('relative', 40),
+            height=('relative', 30)
+        )
+        content_frame.body = content
+
+        self._w = content_frame
+
+    def handle_input(self, button):
+        if button.label == "Yes":
+            # TODO: run "rc-service sshd start"
+            raise urwid.ExitMainLoop()
+        elif button.label == "No":
+            # TODO: move to next dialog.
+            self.gli_widget.cycle_dialog(IntroDialog(self.gli_widget))
 
 
 class GLI(urwid.WidgetPlaceholder):
@@ -99,19 +145,22 @@ class GLI(urwid.WidgetPlaceholder):
         body_filler = urwid.Filler(body_text, valign='middle')
         body_padding = urwid.Padding(
             body_filler,
-            left = 1,
-            right = 1
+            left=1,
+            right=1
         )
 
         body = urwid.AttrMap(urwid.LineBox(body_padding), 'exit')
 
-        no = urwid.Button('No', self.reset_layout)
-        no = urwid.AttrWrap(no, 'efoc', 'esel')
+        cont = urwid.Button('Continue', self.choice)
+        cont = urwid.AttrWrap(cont, 'efoc', 'esel')
 
-        yes = urwid.Button('Yes', self.exit_gli)
-        yes = urwid.AttrWrap(yes, 'efoc', 'esel')
+        quit = urwid.Button('Quit', self.choice)
+        quit = urwid.AttrWrap(quit, 'efoc', 'esel')
 
-        footer = urwid.GridFlow([no, urwid.Divider(), yes], 8, 1, 1, 'center')
+        rs = urwid.Button('Restart', self.choice)
+        rs = urwid.AttrWrap(rs, 'efoc', 'esel')
+
+        footer = urwid.GridFlow([cont, quit, rs], 12, 1, 22, 'center')
 
         layout = urwid.Frame(
             body,
@@ -131,19 +180,23 @@ class GLI(urwid.WidgetPlaceholder):
 
         self.loop.widget = w
 
-    def exit_gli(self, button):
-        raise urwid.ExitMainLoop()
-
-    def reset_layout(self, button):
-        self.loop.widget = self
-        self.loop.draw_screen()
+    def choice(self, button):
+        if button.label == 'Continue':
+            self.loop.widget = self
+            self.loop.draw_screen()
+        elif button.label == 'Quit':
+            raise urwid.ExitMainLoop()
+        elif button.label == 'Restart':
+            self.cycle_dialog(IntroDialog(self))
+            self.loop.widget = self
+            self.loop.draw_screen()
 
     def handle_input(self, key):
         if key in ('esc', 'ESC', 'q', 'Q'):
             self.quit_popup(
                 [
-                    'You hit the ESC key or Q!\n\n',
-                    'Do you really want to quit GLI?'
+                    'You\'ve hit the ESC key/Q!\n\n',
+                    'What do you want to do?'
                 ]
             )
 
