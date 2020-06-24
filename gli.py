1f52ac8b145e234434ba2b4a069c9f6c5115613e
Patrice Clement <monsieurp@gentoo.org>
2020-06-24 09:15:12 +0200

    rework UI entirely

--- gli.py
+++ gli.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 import urwid
-import sys
 
 
-class Intro:
-    palette = [
-        ('titlebar', 'white,bold', 'dark green'),
-        ('intro', 'white', 'dark blue'),
-        ('filler', 'white', 'dark blue'),
-        ('focus', 'white', 'dark blue'),
-        ('selectable', 'black', 'dark green')
-    ]
+PALETTE = [
+    ('titlebar',    'black',    'dark blue'),
+    ('fruit',       'white',    'dark blue'),
+    ('bgcolor',     'black',    'dark blue'),
+    ('selectable',  'white',    'light cyan'),
+    ('focus',       'black',    'light cyan'),
+    ('exit',        'white',    'dark cyan')
+]
 
-    text = """Welcome to the Gentoo Linux Installer!
+
+class IntroDialog(urwid.WidgetWrap):
+    dialog_text = """Welcome to the Gentoo Linux Installer!
 
 GLI simplifies the installation of Gentoo Linux as described
 in the Gentoo Linux handbook.
@@ -21,43 +22,69 @@ See https://wiki.gentoo.org/wiki/Handbook for further information.
 
 Would you like to begin the installation with GLI?"""
 
-    def returncode(self, bt):
-        sys.exit(bt.rc)
+    def __init__(self):
+        self.header = urwid.AttrMap(
+            urwid.Text('GLI - Gentoo Linux Installer'),
+            'titlebar'
+        )
 
-    def main(self):
-        self.header = urwid.Text('GLI - Gentoo Linux Installer')
-        self.header = urwid.AttrMap(self.header, 'titlebar')
+        content_frame = urwid.Frame(body=None)
 
-        self.intro = urwid.Text(self.text)
-        self.intro = urwid.AttrMap(self.intro, 'intro')
+        yes = urwid.AttrMap(urwid.Button('Yes'), 'focus', 'selectable')
+        yes.rc = 0
 
-        self.yes = urwid.Button('Yes', self.returncode)
-        self.yes.rc = 0
-        self.yes = urwid.AttrMap(self.yes, 'focus', 'selectable')
+        no = urwid.AttrMap(urwid.Button('No'), 'focus', 'selectable')
+        no.rc = 1
 
-        self.no = urwid.Button('No', self.returncode)
-        self.no.rc = 1
-        self.no = urwid.AttrMap(self.no, 'focus', 'selectable')
+        buttons = urwid.GridFlow([yes, no], 10, 3, 1, 'center')
 
-        self.buttons = [self.yes, self.no]
-        self.buttons = urwid.GridFlow(self.buttons, 10, 3, 1, 'center')
-
-        self.intro = urwid.Pile([self.intro, urwid.Divider(), self.buttons], focus_item=2)
-        self.intro = urwid.Filler(self.intro, valign='top', top=1, bottom=1)
-        self.intro = urwid.AttrMap(self.intro, 'filler')
-
-        self.sfill = urwid.AttrMap(urwid.SolidFill(' '), 'filler')
-
-        self.intro = urwid.LineBox(self.intro)
-        self.intro = urwid.AttrMap(self.intro, 'filler')
-        self.intro = urwid.Overlay(self.intro, self.sfill,
+        content = urwid.Text(self.dialog_text)
+        content = urwid.Padding(content,
+            align='center', width='pack'
+        )
+        content = urwid.Pile([content, urwid.Divider(), buttons], focus_item=2)
+        content = urwid.AttrMap(urwid.Filler(content, valign='middle', top=0,
+            bottom=0), 'bgcolor')
+        content = urwid.AttrMap(urwid.LineBox(content), 'bgcolor')
+        content = urwid.Overlay(
+            content,
+            urwid.AttrMap(urwid.SolidFill('\N{LIGHT SHADE}'), 'bgcolor'),
             align='center', valign='middle',
-            width=('relative', 60), height=('relative', 40))
+            width=('relative', 60),
+            height=('relative', 40)
+        )
+        content_frame.body = content
 
-        self.layout = urwid.Frame(header=self.header, body=self.intro)
-        self.main = urwid.MainLoop(self.layout, self.palette)
-        self.main.run()
+        self._w = content_frame
+
+
+class GLI(urwid.WidgetPlaceholder):
+    def __init__(self, dialog):
+        super(GLI, self).__init__(
+            urwid.Frame(
+                body=dialog._w,
+                header=dialog.header
+            )
+        )
+
+    def cycle_dialog(self, dialog):
+        self.original_widget = urwid.Frame(
+            body=dialog._w,
+            header=dialog.header
+        )
+
+
+class Main:
+    def __init__(self):
+        self.view = GLI(IntroDialog())
+        self.loop = urwid.MainLoop(self.view, PALETTE,
+            unhandled_input=self.unhandled_input)
+        self.loop.run()
+
+    def unhandled_input(self, key):
+        if key == 'f8':
+            raise urwid.ExitMainLoop()
 
 
 if __name__ == '__main__':
-    Intro().main()
+    Main()
