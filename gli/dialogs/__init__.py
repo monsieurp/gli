16ad17ce2545a7abc652a22546746815849fc3db
Patrice Clement <monsieurp@gentoo.org>
2020-08-12 22:20:05 +0200

    add DiskFormatDialog to list of imports

--- gli/dialogs/__init__.py
+++ gli/dialogs/__init__.py
@@ -1,4 +1,5 @@
 from .diskpart import DiskPartitioningMethodDialog
 from .disksel import DiskSelectionDialog
+from .diskfmt import DiskFormatDialog
 from .intro import IntroDialog
 from .ssh import SSHDialog
